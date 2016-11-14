b617d1525ee8e5685b2637c57434ff2c1c2166ed
Andrew Accuardi and Haley Swain <andrew@>
2016-11-14 13:23:31 -0800

    Add logic to replace necessary values with ping pong or pingpong.

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -3,9 +3,18 @@ class Fixnum
   define_method(:ping_pong) do
     numbers = []
     total = 0
+
     self.times() do |time|
       total = time + 1
-      numbers = numbers.push(total)
+      if total % 15 === 0
+        numbers.push("pingpong")
+      elsif total % 3 === 0
+        numbers.push("ping")
+      elsif total % 5 === 0
+        numbers.push("pong")
+      else
+      numbers.push(total)
+      end
     end
     numbers
   end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -2,8 +2,15 @@ require('ping_pong')
 
 describe('Fixnum#ping_pong') do
   it("takes an inputted number from the user and creates an array up to that number") do
-    expect((5).ping_pong()).to(eq([1, 2, 3, 4, 5]))
+    expect((2).ping_pong()).to(eq([1, 2]))
+  end
+  it("replaces numbers divisible by 3 with ping") do
+    expect((3).ping_pong()).to(eq([1, 2, "ping"]))
+  end
+  it("replaces numbers divisible by 5 with pong") do
+    expect((5).ping_pong()).to(eq([1, 2, "ping", 4, "pong"]))
+  end
+  it("replaces numbers divisible by 15 with pingpong") do
+    expect((15).ping_pong()).to(eq([1, 2, "ping", 4, "pong", "ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong"]))
   end
 end
-
-describe
